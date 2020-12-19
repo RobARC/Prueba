d7b218ca7a5ce857cdcc0486f1314c7fa0c941ea
Roberth <rrondoc@gmail.com>
2020-12-19 15:16:21 -0500

    Test proyect 0x19C

---
+++ monty/monty.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/*
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+}
+char DirFile;
+FILE *OPFile;
+
+DirFile = "/home/monty/bytecode/"
+
+
+if (argc != 2)
+	printf(STDOUT_FILENO, "Error: Can't open file %s\n");
+	exit(EXIT_FAILURE);
+
+OPFile = fopen(argv[1], "r");
+
+
+
+
+
+
+
+
+
+}
