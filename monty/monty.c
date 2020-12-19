02e3a5e363a9fb9fd4cf4af32d890e72e08f560b
Roberth <rrondoc@gmail.com>
2020-12-19 16:13:50 -0500

    prueba abrir archivo

--- monty/monty.c
+++ monty/monty.c
@@ -5,25 +5,27 @@
  *
  */
 int main(int argc, char *argv[])
-}
-char DirFile;
+{
+	
 FILE *OPFile;
-
-DirFile = "/home/monty/bytecode/"
 
 
 if (argc != 2)
-	printf(STDOUT_FILENO, "Error: Can't open file %s\n");
-	exit(EXIT_FAILURE);
-
+{	
+	printf("Error: Can't open file");
+	exit(0);
+}
 OPFile = fopen(argv[1], "r");
 
+if (OPFile == NULL)
+{
+puts("Error al abrir el archivo.");
+exit(1);
+}
+else
+puts("Abrio archivo");
 
-
-
-
-
-
+return 0;
 
 
 }
